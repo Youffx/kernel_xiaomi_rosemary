2eaf14bd81f770047f84d4ae9d0a35074d406dc6
Youff <asa.jazal@gmail.com>
2026-08-08 16:30:42 +0000

    Revert "build: fix Clang 20 errors breaking CI build"
    This reverts commit 22686f1ad599b128175691fdd52e8ad0b00590a5.

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -3766,12 +3766,12 @@ try_onemore:
 		err = f2fs_recover_fsync_data(sbi, true);
 		if (err > 0) {
 			if (!f2fs_readonly(sb)) {
-				f2fs_err(sbi,
+				f2fs_msg(sb, KERN_ERR,
 					"Need to recover fsync data");
 				err = -EINVAL;
 				goto free_meta;
 			} else {
-				f2fs_info(sbi,
+				f2fs_msg(sb, KERN_INFO,
 					"drop all fsynced data");
 				err = 0;
 			}
