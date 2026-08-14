664082b5ca4d1c4dfdacc3f031607a688f221aee
Tiezhu Yang <yangtiezhu@loongson.cn>
2026-08-14 05:58:31 +0000

    lib: make LZ4_decompress_safe_forceExtDict() static
    LZ4_decompress_safe_forceExtDict() is only used in
    lib/lz4/lz4_decompress.c, make it static to fix the build warning about
    "no previous prototype" [1].
    
    [1] https://lore.kernel.org/lkml/202206260948.akgsho1q-lkp@intel.com/
    
    Link: https://lkml.kernel.org/r/1656298965-8698-1-git-send-email-yangtiezhu@loongson.cn
    Signed-off-by: Tiezhu Yang <yangtiezhu@loongson.cn>
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Youffx <asa.jazal@gmail.com>

--- lib/lz4/lz4_decompress.c
+++ lib/lz4/lz4_decompress.c
@@ -504,7 +504,7 @@ static int LZ4_decompress_safe_withSmallPrefix(const char *source, char *dest,
 				      (BYTE *)dest - prefixSize, NULL, 0);
 }
 
-int LZ4_decompress_safe_forceExtDict(const char *source, char *dest,
+static int LZ4_decompress_safe_forceExtDict(const char *source, char *dest,
 				     int compressedSize, int maxOutputSize,
 				     const void *dictStart, size_t dictSize)
 {
